b811f211e17123a8a3848add03bb86c9b2a9b44a
DeviantKay <adidtyaputra@gmail.com>
2023-05-04 11:59:08 +0700

    awal

---
+++ assets/js/Export/mobilemenu.js
@@ -0,0 +1,9 @@
+/*=============== REMOVE MENU MOBILE JAVASCRIPT ===============*/
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+
+export default linkAction;
---
+++ assets/js/Export/scrollreveal.js
@@ -0,0 +1,10 @@
+/*=============== SCROLL REVEAL JAVASCRIPT ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+export default sr;
